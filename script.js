ed7ab59d1d8b9ffce53d473b158e4872d9b29604
Daniel Sayeg <sayegdaniel@gmail.com>
2026-07-19 14:48:18 +0300

    page 11

--- script.js
+++ script.js
@@ -127,7 +127,8 @@ const INLINE_COMIC_DATA = {
         "assets/comic/chapter-4/page-07.jpg",
         "assets/comic/chapter-4/page-08.jpg",
         "assets/comic/chapter-4/page-09.jpg",
-        "assets/comic/chapter-4/page-10.jpg"
+        "assets/comic/chapter-4/page-10.jpg",
+        "assets/comic/chapter-4/page-11.jpg"
       ]
     }
   ]
